209c1441f5fd26b4ce597577adcb40219a35c788
Vladislav <kaidalov.007@yandex.ru>
2022-04-22 21:08:24 +0300

    Relise 2.0.3

--- app.js
+++ app.js
@@ -537,7 +537,7 @@ async function start() {
 
 
 
-                                const {amount} = walletSum.filter(item => item.transactType === "Deposit")[0] || 0
+                                const {amount} = walletSum.filter(item => item.transactType === "Deposit")[0] || -2
 
 
                                 // проверка депозита
